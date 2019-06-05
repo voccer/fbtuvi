a7c2f9127c0eab31571bb18ebb9ccf0da49fcf19
Ducvoccer <ntduc250798@gmail.com>
2019-06-05 22:57:24 +0700

    abc

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def main():
     finally:
         pass
 
-    client = VoccerBot('nduc535@gmail.com', 'traduc20', session_cookies='')
+    client = VoccerBot('voccer123@gmail.com', 'ducquang', session_cookies='')
 
     # Lấy session và lưu vào file để lần sau dùng cho đăng nhập
     session_cookies_new = client.getSession()
