7da099c54796b7669171c5f899ef336393b7c578
Ducvoccer <ntduc250798@gmail.com>
2020-07-13 15:32:10 +0700

    add lihc

--- bot.py
+++ bot.py
@@ -1,15 +1,17 @@
 # -*- coding: UTF-8 -*-
 from fbchat.models import *
 from fbchat import log, Client
-
+import datetime
 from tu_vi import TuVi
+from lunarcalendar import Converter, Solar, Lunar
 
+
+other_text = '🙂 Xin Chào. Tôi là Bot chat của Trong Duc - Voccer. \n- Hiện tại anh ấy không thể rep tin nhắn ngay được. \n- Nếu xem tử vi gõ /tuvi <tuổi>; ví dụ: /tuvi sửu. \n-Nếu xem cung hoàng đạo gõ /hoangdao <cung>; \n ví dụ: /hoangdao song ngư\n- xem lịch gõ /lich'
 
 class VoccerBot(Client):
     def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
         self.markAsDelivered(thread_id, message_object.uid)
         self.markAsRead(thread_id)
-
         log.info("{} from {} in {}".format(message_object, thread_id, thread_type.name))
         # log.info("hello")
         if author_id != self.uid:
@@ -28,10 +30,17 @@ class VoccerBot(Client):
                     loi_phan = tuvi.cung_hoang_dao(cung_hd=cung)
                     # print('loi phan cua toi la {}'.format(loi_phan))
                     self.send(Message(text=loi_phan), thread_id=thread_id, thread_type=thread_type)
+                elif '/lich' in message_object:
+                    solar_today = datetime.date.today()
+                    lunar_today = Converter.Solar2Lunar(Solar(solar_today.year, solar_today.month, solar_today.day))
+                    self.send(Message(
+                        text="Hôm nay dương lịch: {}, âm lịch ngày {}".format(solar_today, lunar_today)),
+                        thread_id=thread_id,
+                        thread_type=thread_type
+                    )
                 else:
                     self.send(Message(
-                        text='\n \n🙂 Xin Chào. Tôi là Bot chat của Trong Duc - Voccer. \n- Hiện tại anh ấy không thể rep tin nhắn ngay được. \n- Nếu xem tử vi gõ /tuvi <tuổi>; ví dụ: /tuvi sửu. \n-Nếu xem cung hoàng đạo gõ /hoangdao <cung>; \n ví dụ: /hoangdao song ngư\n- Tin nhắn của bạn: {0}'.format(
-                            message_object.text)),
+                        text=),
                         thread_id=thread_id,
                         thread_type=thread_type
                     )
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import json
-
+import request
 from bot import VoccerBot
+import datetime
+from lunarcalendar import Converter, Solar, Lunar
 
+def get_birthday():
+    r = requests.get('https://docs.google.com/document/u/0/export?format=txt&id=1b2aQMfZvhLhqs5bnGrdNZUnOSmCGsuNZeSH8Qk6ndrg&token=AC4w5VjlljBzAvNU8CtCSnvvFwe52p-2-w%3A1589941528767&ouid=102939292131381354747&includes_info_params=true&inspectorResult=%7B%22pc%22%3A1%2C%22lplc%22%3A20%7D')
+    b = r.text
 
 def main(user):
     session_cookies = ''
@@ -22,7 +27,7 @@ def main(user):
 if __name__ == '__main__':
     print('server hoat dong nhe')
     user = '0392571400'
-    main(user)
+    # main(user)
 
 
 
